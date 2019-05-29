953d3b1034068dfb477e781b68ba78744e9d45e6
sharpiro <dsharpbb09@gmail.com>
2019-05-29 18:09:08 -0400

    multiple file types

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,19 @@
-'use strict';
-
 import * as vscode from 'vscode';
+import * as path from "path"
 
 interface Container {
 	uri: vscode.Uri
 	emitter: vscode.EventEmitter<vscode.Uri>
-	// value: string
 }
 
 const containers = new Map<string, Container>();
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
 
-	// register a content provider for the cowsay-scheme
-	const myScheme = 'cowsay';
+	const myScheme = 'vscode-focus';
 	const myProvider = new class implements vscode.TextDocumentContentProvider {
-
-		// emitter and its event
 		onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
 		onDidChange = this.onDidChangeEmitter.event;
-		// private _subscriptions: vscode.Disposable;
 
 		_subscriptions = vscode.workspace.onDidCloseTextDocument(doc => {
 			containers.delete(doc.uri.toString())
@@ -42,48 +36,17 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 	}
 	subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider));
 
-	// register a command that opens a cowsay-document
-	subscriptions.push(vscode.commands.registerCommand('cowsay.send', async () => {
+	subscriptions.push(vscode.commands.registerCommand('vscode-focus.focus', async () => {
 		const activeEditor = vscode.window.activeTextEditor
 		if (activeEditor && !activeEditor.selection.isEmpty) {
-			const uri = vscode.Uri.parse('cowsay:' + "cowsay-send.js");
-			const docs = vscode.workspace.textDocuments
-			// vscode.workspace.docum
-			// let doc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
-			// const isClosed = doc.isClosed
+			const filename = path.basename(activeEditor.document.fileName)
+			const uri = vscode.Uri.parse(`${myScheme}:focus-${filename}`);
 			const container = containers.get(uri.toString())
 			if (container) {
 				container.emitter.fire(uri)
-				return
+				// return
 			}
-			await vscode.window.showTextDocument(uri, { preview: false });
+			await vscode.window.showTextDocument(uri, { viewColumn: vscode.ViewColumn.Two });
 		}
 	}));
-
-	// register a command that opens a cowsay-document
-	subscriptions.push(vscode.commands.registerCommand('cowsay.say', async () => {
-		let what = await vscode.window.showInputBox({ placeHolder: 'cowsay...' });
-		if (what) {
-			let uri = vscode.Uri.parse('cowsay:' + what);
-			
-			// await vscode.window.showTextDocument(doc, { preview: false });
-			await vscode.window.showTextDocument(uri, { preview: false });
-		}
-	}));
-
-	// register a command that updates the current cowsay
-	subscriptions.push(vscode.commands.registerCommand('cowsay.backwards', async () => {
-		if (!vscode.window.activeTextEditor) {
-			return; // no editor
-		}
-		let { document } = vscode.window.activeTextEditor;
-		if (document.uri.scheme !== myScheme) {
-			return; // not my scheme
-		}
-		// get path-components, reverse it, and create a new uri
-		let say = document.uri.path;
-		let newSay = say.split('').reverse().join('');
-		let newUri = document.uri.with({ path: newSay });
-		await vscode.window.showTextDocument(newUri, { preview: false });
-	}))
 }
